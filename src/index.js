a0e9ca83735ba61e99a913d8d3ddd362d37014f9
Foster <adam.foster@education.gov.uk>
2019-11-27 12:20:56 +0000

    fixes build adds browser sync

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import "./styles.css";
 import "./styles.scss";
-console.log("hello world!");
+
+console.log("this is working :)");
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const path = require('path');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 
 const config = {
   entry: './src/index.js',
@@ -35,8 +36,17 @@ const config = {
           'sass-loader'
         ]
       }
-    ]
-  }
+    ],
+  },
+  plugins: [
+    new BrowserSyncPlugin({
+      // browse to http://localhost:3000/ during development,
+      // ./public directory is being served
+      host: 'localhost',
+      port: 3000,
+      server: { baseDir: ['dist'] }
+    })
+  ]
 };
 
 module.exports = config;
